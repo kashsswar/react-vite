d77c504870fc23195c87b2746a3dd8b0f17b0b12
Karishma Sinsinwar <kashsswar@gmail.com>
2024-06-15 00:30:50 +0530

    use of useState hook

--- vite-react/src/Chai.jsx
+++ vite-react/src/Chai.jsx
@@ -1,7 +1,31 @@
+/* eslint-disable no-unused-vars */
+import React, {useState} from "react"
+
+
 function Chai()
 {
+    let [counter, setCounter] = useState(15)
+    const addValue=()=>{
+       if(counter >= 20){
+        alert("You have reached the maximum limit")
+        return
+       }
+       setCounter(counter + 1)
+    }
+    const removeValue =()=>{
+        if(counter<=0){
+            alert("You have reached the minimum limit")
+            return
+        }
+        setCounter(counter - 1)
+    }
     return(
+        <>
         <h1>Chai in react</h1>
+        <h2>Number of chais i drank : {counter}</h2>
+        <button onClick={(addValue)}>Add value</button>
+        <button onClick={(removeValue)}>Remove value</button>
+        </>
     )
 }
 export default Chai
