baefa547c3d1d5650c0068d0a395edcceb75a194
Karishma Sinsinwar <kashsswar@gmail.com>
2024-06-16 17:04:43 +0530

    Created project for background changer

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,12 +1,14 @@
 import Chai from './Chai'
 // import CustomReact from './CustomReact'
 import Tailwind from './Tailwind'
+import BgChanger from './BgChanger'
 function App(){
   return(
     <>
    {/* <Chai/> */}
     {/* <CustomReact/>*/}
-    <Tailwind success="Dar ke age jeet hai"/>
+    {/* <Tailwind success="Dar ke age jeet hai"/> */}
+    <BgChanger/>
   </> 
   )
 
